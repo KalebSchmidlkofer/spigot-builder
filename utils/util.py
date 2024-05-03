1924f24207d11c143b0193b215a2b6e01d533e8b
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-05-03 14:01:57 -0700

    update: added utils that fetches vanilla and paper versions

---
+++ utils/util.py
@@ -0,0 +1,24 @@
+import requests
+
+def get_paper_versions():
+  vapi='https://api.papermc.io/v2/projects/paper'
+  vrequest=requests.get(vapi)
+  vdata=vrequest.json()
+  versions=vdata['versions']
+  return versions
+
+def get_vanilla_version():
+  vapi='https://launchermeta.mojang.com/mc/game/version_manifest.json'
+  vrequest=requests.get(vapi)
+  vdata=vrequest.json()
+  versions=vdata['versions']
+  return versions
+
+
+
+
+print(get_paper_versions())
+print(get_vanilla_version())
+
+
+
