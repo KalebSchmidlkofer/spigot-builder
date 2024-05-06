6129953ad56025861ab2b25e2407a9fbb34b8aa4
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-05-05 21:36:18 -0700

    update: better loguru logging

--- builders/spigotbuilder.py
+++ builders/spigotbuilder.py
@@ -1,12 +1,13 @@
 import subprocess
 import utils
 from loguru import logger
-
+#? Is this legal?
 
 def _version_to_tuple(version_str):
   try:
     return tuple(map(int, version_str.split('.')))
   except ValueError as e:
+    logger.debug(e)
     logger.warning(f'Failed to parse: {version_str}')
     logger.info(f'Use Verbose to view more info')
     pass
@@ -22,19 +23,18 @@ def build(build_path: str, version = None):
     version=utils.get_paper_versions()
     VERSIONS = [_version_to_tuple(versions) for versions in version]
   else:
-
     VERSIONS = [_version_to_tuple(version)]
   
   for x in VERSIONS:
     if x == None:
       pass
     version = _tuple_to_version(x)
-    print(f'Building Version: {version}')
+    logger.trace(f'Bulding Version: {version}')
     if x > (1,17,1):
      subprocess.run(["docker", "run", "--rm", "-v", f"{build_path}:/release:z", "-e", f"VERSION={version}", "--name", f"spigot-builder-{version}", "zastrix/spigot-builder:openjdk-17-alpine"])
     elif x == (1,17,1):
       subprocess.run(["docker", "run", "--rm", "-v", f"{build_path}:/release:z", "-e", f"VERSION={version}", "--name", f"spigot-builder-{version}", "zastrix/spigot-builder:openjdk-16-alpine"])
     elif x < (1,17,1):
       subprocess.run(["docker", "run", "--rm", "-v", f"{build_path}:/release:z", "-e", f"VERSION={version}", "--name", f"spigot-builder-{version}", "zastrix/spigot-builder:openjdk-8-alpine"])
-
+    logger.trace('Making api call')
     #* After building, have to make an api call to the website with the file which is going to be os.path.join(build_path, f'spigot-{version}.jar')
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from typer import Typer, Option, Argument
-from typing_extensions import Annotated
+from typing_extensions import Annotated, Required
 import utils
 import builders
 from os import path
 from sys import stderr
 from loguru import logger
+from typing import Optional
 
 
 
@@ -14,32 +15,43 @@ app = Typer(no_args_is_help=True, add_completion=False)
 build_path=path.join('.', 'release')
 
 
+def loguru_init(trace):
+  logger.remove(0)
+  if trace:
+    logger.add(stderr, level='TRACE')
+  else:
+    logger.add(stderr, level='INFO')
+
+
+
 @app.command('spigot')
 def build_spigot(
-  version: list[str] = Argument(),
-  trace: Annotated[bool, Option("--trace", "-t", help='Enable trace messages')] = False
+  version: Optional[list[str]] = Argument(default=None),
+  trace: Annotated[bool, Option("--trace", "-t", help='Enable trace-level Debugging')] = False
 ):
+  loguru_init(trace)
+  logger.info('building spigot')
   builders.spigotbuilder.build(build_path, version=version)
 
 
 
 @app.command('paper')
 def build_paper(
-  version: list[str] = Argument(),
+  version: Optional[list[str]] = Argument(default=None),
   trace: Annotated[bool, Option("--trace", "-t", help='Enable trace messages')] = False
   ):
+  loguru_init(trace)
+  logger.info('building paper')
   builders.paperbuilder.build(build_path, version=version)
+
+
+
 
 @app.command('test')
 def test(
   trace: Annotated[bool, Option("--trace", "-t", help='Enable trace messages')] = False
   ):
-  logger.remove(0)
-  if trace:
-    logger.add(stderr, level='TRACE')
-  else:
-    logger.add(stderr, level='INFO')
-    
+  loguru_init(trace)
   logger.debug('this is debug')
   logger.trace('this is trace')
   logger.warning('this is warning')
